1e2c0a5e239390bbf4ea85398c2efaff8692c47b
totalintelli <blueintelli@gmail.com>
2016-05-23 11:41:32 +0900

    파일 생성 및 원 그리기 완료

---
+++ WPFForm/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFForm
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            this.WindowState = System.Windows.WindowState.Normal;
+            this.WindowStyle = System.Windows.WindowStyle.ToolWindow;
+            this.Width = 800;
+            this.Height = 600;
+            this.Title = "WPFForm";
+        }
+    }
+
+    class CustomDrawing : Canvas
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            // 너비
+            double Width = this.ActualWidth;
+
+            // 높이
+            double Height = this.ActualHeight;
+
+            // 샘플 그리기
+            dc.DrawLine(new Pen(Brushes.Black, 2), new Point(0, 0), new Point(100, 100));
+            
+            // 실제 드로잉 영역
+
+
+
+
+
+            //**************************
+
+
+
+
+            base.OnRender(dc);
+        }
+
+    }
+
+}
---
+++ WinForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Width = 800;
+            this.Height = 600;
+            this.Text = "WinForm";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // 너비
+            float Width = panel1.Width;
+
+            // 높이
+            float Height = panel1.Height;
+
+            // 실제 드로잉 영역
+            // 마진
+            Padding RectMargin = new System.Windows.Forms.Padding(30, 30, 30, 30);
+            // 드로잉 너비
+            float DrawingWidth = (panel1.ClientRectangle.Width - (RectMargin.Right + RectMargin.Left)) / 5;
+            // 드로잉 높이
+            float DrawingHeight = (panel1.ClientRectangle.Height - (RectMargin.Top + RectMargin.Bottom)) / 3;
+            // 드로잉 너비의 두 배
+            float DrawingWidthDouble = DrawingWidth * 2f;
+            // 원을 그리는 사각형
+            RectangleF CircleRect = new RectangleF(RectMargin.Left, RectMargin.Top, DrawingWidth, DrawingHeight);
+
+            // 원을 그린다.
+            e.Graphics.FillEllipse(new SolidBrush(Color.Purple), CircleRect);
+
+
+
+            //**************************
+        }
+    }
+}
